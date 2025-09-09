5c25bc81c4720e924d4e50ff3c4483ef686d988f
mouad ziyani <ziyanimouad@gmail.com>
2025-09-09 17:15:52 +0200

    challenge6

---
+++ challenge6.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+long long fibonacci(int n){
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+
+    return fibonacci(n-1) + fibonacci(n-2);
+}
+
+int main(){
+
+    int n ;
+    printf(" entrer un nombre : ");
+    scanf("%d",&n);
+
+    printf("Fibonacci(%d) = %lld\n", n, fibonacci(n));
+
+    return 0;
+}
