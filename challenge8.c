caa2fd6e44ba6b20cb0b5b02af67798fbcc3ed6b
mouad ziyani <ziyanimouad@gmail.com>
2025-09-09 17:21:48 +0200

    challenge8

---
+++ challenge8.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int est_pair(int nombre) {
+    if (nombre % 2 == 0) {
+        return 1;
+    }
+    return 0;
+}
+
+int main() {
+    int n;
+
+    printf("Entrer un nombre : ");
+    scanf("%d", &n);
+
+    if (est_pair(n)) {
+        printf("%d est pair.\n", n);
+    } else {
+        printf("%d est impair.\n", n);
+    }
+
+    return 0;
+}
